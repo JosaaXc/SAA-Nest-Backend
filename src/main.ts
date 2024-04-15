0cba39ef22c34dbcb7f33810d6da67ad4446711f
JosaaXC <114438741+JosaaXc@users.noreply.github.com>
2024-04-14 22:57:52 -0600

    feat(cors): enable CORS in the application

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   const logger = new Logger('bootstrap')
 
   app.setGlobalPrefix('api');
